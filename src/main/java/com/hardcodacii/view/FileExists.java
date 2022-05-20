277d699e36eaa64826eaa1b32429752a29ef5e9f
Dumitru Sandulache <sandulachedumitru@hotmail.com>
2022-05-20 19:21:53 +0300

    S2 - check if a file exists

--- src/main/java/com/hardcodacii/view/FileExists.java
+++ src/main/java/com/hardcodacii/view/FileExists.java
@@ -6,36 +6,30 @@ import static com.hardcodacii.view.Show.showln;
 import static com.hardcodacii.view.Show.showlnErr;
 
 /**
- *
- * @author Sandulache Dumitru
- * 
+ * @author Sandulache Dumitru (sandulachedumitru@hotmail.com)
  */
 public class FileExists {
     /**
-     * 
      * @param path of type String (path of data file)
      * @return boolean true if file exist or false if file do not exist or path is a directory
-     * 
      */
     public boolean exists(String path) {
         boolean fileExists = false;
-	Path pathSource = Paths.get(path);
-	if(Files.exists(pathSource, LinkOption.NOFOLLOW_LINKS)) {
-            showln("The file/directory \"" + pathSource.getFileName() + "\" exists");			
+
+        Path pathSource = Paths.get(path);
+        if (Files.exists(pathSource, LinkOption.NOFOLLOW_LINKS)) {
+            showln("The file/directory \"" + pathSource.getFileName() + "\" exists");
             // check whether it is a file or a directory
-            if(Files.isDirectory(pathSource, LinkOption.NOFOLLOW_LINKS)) {
-		showlnErr("\"" + pathSource.getFileName() + "\" is a directory. Need a file.");
-		//if directory then teminate app
-            }
-            else {
-		showln("\"" + pathSource.getFileName() + "\" is a file");
+            if (Files.isDirectory(pathSource, LinkOption.NOFOLLOW_LINKS)) {
+                showlnErr("\"" + pathSource.getFileName() + "\" is a directory. Need a file.");
+                //if directory then teminate app
+            } else {
+                showln("\"" + pathSource.getFileName() + "\" is a file");
                 fileExists = true;
             }
-	}
-	else {
+        } else
             showlnErr("The file \"" + pathSource.getFileName() + "\" does not exist");
-	}
-        
+
         return fileExists;
-    }//end method
-}//end class
+    }
+}
